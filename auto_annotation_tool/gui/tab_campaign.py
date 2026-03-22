2b5a3b2d75e2dec81d0439e0ee506728b618f1e1
rszuder <rszuderski@gmail.com>
2026-03-22 23:13:49 +0100

    Kontrola synchronizacji: stan przed spięciem treningu znaków z kolejną iteracją

--- auto_annotation_tool/gui/tab_campaign.py
+++ auto_annotation_tool/gui/tab_campaign.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Zakładka: Rozkład Jazdy (Dashboard Kampanii MLOps).
+Zakładka: Rozkład Jazdy (Dashboard Kampanii MLOps)..
 """
 
 import tkinter as tk
